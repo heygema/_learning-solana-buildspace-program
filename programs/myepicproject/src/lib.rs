b6e40fdaa59d63d8cfa6f6816fa9eaef55f283bf
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2021-11-10 00:19:47 +0700

    added addGif counter

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -26,20 +26,28 @@ pub mod myepicproject {
         base_account.total_gifs = 0;
         Ok(())
     }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct StartStuffOff<'info> {
     #[account(init, payer = user, space = 9000)]
     pub base_account: Account<'info, BaseAccount>,
-    // #[account(init, payer = user, space = 9000)]
-    // pub base_account: Account<'info, BaseAccount>,
+
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
-    // #[account(mut)]
-    // pub user: Signer<'info>,
-    // pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
 }
 
 #[account]
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -41,6 +41,15 @@ const main = async () => {
   // Fetch data from the account.
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log('GIF Count >>>', account.totalGifs.toString());
+
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('(Updated) GIF Count >>>', account.totalGifs.toString());
 };
 
 const runMain = async () => {
